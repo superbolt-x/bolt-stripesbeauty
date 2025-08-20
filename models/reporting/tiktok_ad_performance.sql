56f48936bb9cd6220eb5a3620db87adea7781422
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-08-20 14:08:43 -0600

    Initial commit

---
+++ models/reporting/tiktok_ad_performance.sql
@@ -0,0 +1,26 @@
+{{ config (
+    alias = target.database + '_tiktok_ad_performance'
+)}}
+
+SELECT 
+campaign_name,
+campaign_id,
+campaign_status,
+campaign_type_default,
+adgroup_name,
+adgroup_id,
+adgroup_status,
+audience,
+ad_name,
+ad_id,
+ad_status,
+visual,
+date,
+date_granularity,
+cost as spend,
+impressions,
+clicks,
+complete_payment as purchases,
+total_complete_payment_rate as revenue,
+web_event_add_to_cart as atc
+FROM {{ ref('tiktok_performance_by_ad') }}
